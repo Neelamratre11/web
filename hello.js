3e12e460e4fe1bff0535f43be0fb577bf9f9be75
Neelamratre11 <154513305+Neelamratre11@users.noreply.github.com>
2024-04-12 19:05:44 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text",
+{
+    strings: ["Frontend Developer", "YouTuber","Blogger"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+
+}) 
